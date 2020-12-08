e2d65f064225b0a00dff730c6cfcb3b88c944b9d
Khari Johnson <kj@gvempire.com>
2020-12-08 06:37:50 -0500

    fiz: optomize \n\n
    Had to remove puppCall becasue it was taking to create new browsers everytime.
    This mean I had to add a check to Crawler.crawl() itself to check out of the loop and close the browser.
    
    It's much faster, but there's still a long wasy to go before any actualy features. Though It's really cool and I'm enjoying making it right now.

--- src/index.ts
+++ src/index.ts
@@ -13,14 +13,16 @@ export const onError = (error: Error) => {
   throw error;
 };
 export const onSuccess = (msg: string) =>
-  console.log(successColor('✅: ' + msg));
+  console.log(successColor('✅ : ' + msg));
 export const onInfo = (msg: string) =>
-  console.info(infoColor('🚀: ' + msg));
+  console.info(infoColor('🚀 : ' + msg));
 export const onWarning = (msg: string) =>
-  console.info(warningColor('🚧:' + msg));
+  console.info(warningColor('🚧 : ' + msg));
 
 export class Crawler {
   constructor(domain: string = process.argv[2]) {
+    onSuccess('Good Vibes and greater Ventures');
+
     if (!domain) {
       onError(new Error('Domain is required'));
     }
@@ -35,12 +37,14 @@ export class Crawler {
 
     this.domain = domain;
     this.domainName = domain.split('//')[1].split('.')[0];
+    this.allRoutes = [domain];
     this.crawl(domain);
   }
 
   domain: string;
   domainName: string;
-  routes: string[] = [];
+  pastRoutes: (string | null)[] = [];
+  allRoutes: (string | null)[];
   browser: puppeteer.Browser | null = null;
   page: puppeteer.Page | null = null;
 
@@ -51,13 +55,13 @@ export class Crawler {
   async init() {
     // open a headless browser
     if (!this.browser) {
-      onInfo('Browser Opened');
-      this.browser = await puppeteer.launch({ headless: false });
+      onSuccess('Browser Opened');
+      this.browser = await puppeteer.launch({ headless: true });
     }
 
     // open a new tab
     if (!this.page) {
-      onInfo('Tab Opened');
+      onSuccess('Tab Opened');
       this.page = await this.browser.newPage();
     }
 
@@ -68,14 +72,14 @@ export class Crawler {
     const { page } = await this.init();
 
     try {
-      if (!this.routes.includes(path)) {
-        // goto page and add to routes
+      if (!this.pastRoutes.includes(path)) {
+        // goto page and add to pastRoutes
         onInfo(
-          `Visiting ${path}: ${this.routes.indexOf(path)}/${
-            this.routes.length
-          } routes`,
+          `Visiting ${path}: ${this.pastRoutes.indexOf(path)}/${
+            this.allRoutes.length
+          }`,
         );
-        this.routes.push(path);
+        this.pastRoutes.push(path);
         await page.goto(path, { waitUntil: 'load', timeout: 0 });
 
         // take a screenshot
@@ -89,46 +93,60 @@ export class Crawler {
         });
 
         // gather local links
-        onInfo(`Collecting Routes on ${path}`);
+        onInfo(`Collecting pastRoutes on ${path}`);
+
         let localRoutes = await page?.evaluate(() =>
           Array.from(
-            document.querySelectorAll('a[href^="/"]'),
+            document.querySelectorAll("a[href^='/']"),
             (element) => element.getAttribute('href'),
           ),
         );
 
         localRoutes = localRoutes
           ?.filter((route) => route !== '/')
-          .filter((route) => !this.routes.includes(route || ''));
+          .filter((route) => !this.pastRoutes.includes(route || ''));
 
         onInfo(
           `Routes found at ${page?.url()}\n` +
             localRoutes.map((route) => `    - ${route}`).join('\n'),
         );
 
-        // forEach link crawl if not in routes already
-        for (let i = 0; i < localRoutes.length; i++) {
-          const localRoute = localRoutes[i];
-
-          await this.crawl(this.domain + localRoute);
-        }
-
-        onInfo(
-          `All Routes: \n` +
-            this.routes.map((route) => `    - ${route}`).join('\n'),
+        this.allRoutes.push(
+          ...localRoutes.filter(
+            (route) => this.allRoutes.indexOf(route || '') === -1,
+          ),
         );
 
-        return localRoutes;
+        // serializes
+        await localRoutes.reduce(async (memo, route) => {
+          await memo;
+
+          await this.crawl(this.domain + route);
+        }, this.wait(50));
+
+        if (
+          this.allRoutes
+            .slice(1)
+            .every(
+              (route) =>
+                this.pastRoutes.indexOf(`${this.domain}${route}`) !==
+                -1,
+            )
+        ) {
+          // close the tab
+          onWarning('Tab Closed');
+          this.page = null;
+          await page?.close();
+
+          // close the browser window
+          onWarning('Browser Closed');
+          return this.browser?.close();
+        }
       } else {
         return onWarning(path + ' has already been visited');
       }
     } catch (error) {
       onError(error);
-    } finally {
-      // close the browser window
-      onInfo('Tab Closed');
-      await page?.close();
-      this.page = null;
     }
   }
 }
